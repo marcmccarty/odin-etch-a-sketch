71ad427f76f001be7feafca87776c5e74c27863f
Marc McCarty <maruku115@gmail.com>
2024-05-29 21:22:15 -0500

    Complete etch-a-sketch functionality

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,6 +1,20 @@
+const newGridButton = document.createElement("button");
 const gridContainer = document.querySelector(".container");
+newGridButton.textContent = "New Grid (1-100)";
+document.body.prepend(newGridButton);
+createGrid();
 
-function createGrid(gridSize) {
+newGridButton.addEventListener("click", () => {
+    newGridSize = parseInt(prompt("Enter a new grid size of 1 to 100:"));
+    if (newGridSize && typeof(newGridSize) == "number" && newGridSize > 0 && newGridSize <= 100) {
+        createGrid(newGridSize);
+    } else {
+        alert("Please enter a number between 1 and 100 next time.");
+    }
+})
+
+function createGrid(gridSize = 16) {
+    destroyGrid();
     for (let i = 0; i < gridSize; i++) {
         const gridRow = document.createElement("div");
         gridRow.id = "row" + (i+1);
@@ -9,6 +23,9 @@ function createGrid(gridSize) {
             const gridColumn = document.createElement("div");
             gridColumn.id = "column" + (j+1);
             gridColumn.classList.add("tile");
+            gridColumn.addEventListener("mouseover", () => {
+                gridColumn.style.background = "black";
+            })
             gridRow.appendChild(gridColumn);
         }
         gridContainer.appendChild(gridRow);
