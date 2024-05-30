f3de9444910f17403fe5330b69de503231f847f2
Marc McCarty <maruku115@gmail.com>
2024-05-29 19:28:46 -0500

    Add initial createGrid() function

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,13 +1,14 @@
 const gridContainer = document.querySelector(".container");
-const gridTile = document.createElement("div");
 
 function createGrid(gridSize) {
     for (let i = 0; i < gridSize; i++) {
         const gridRow = document.createElement("div");
         gridRow.id = "row" + (i+1);
+        gridRow.classList.add("row");
         for (let j = 0; j < gridSize; j++) {
             const gridColumn = document.createElement("div");
             gridColumn.id = "column" + (j+1);
+            gridColumn.classList.add("tile");
             gridRow.appendChild(gridColumn);
         }
         gridContainer.appendChild(gridRow);
